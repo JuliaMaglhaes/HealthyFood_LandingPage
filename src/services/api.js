6d4f604fdc5fea6c95c350f1d731b69645ad397c
Julia Magalhães <juliamagalhaes28@outlook.com>
2021-05-23 21:46:35 -0300

    Remoção pacotes

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: process.env.API_CONNECTION || 'https://viacep.com.br/ws'
+    baseURL: process.env.API_URL || 'https://viacep.com.br/ws'
 })
